6d110cdf1b35169e058697bc460fff557dc96cb1
Big_Bro <anh.bigbrother@gmial.com>
2023-12-13 12:49:45 +0700

    update1

--- src/App.js
+++ src/App.js
@@ -1,61 +1,64 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-let sheetId = '1BIw0A0lpxnuUmT-uRNFM5bzt4fwTeGbkJEdf7q41L4Q';
-let sheetTitle = 'form1';
-
-let fulUrl = ('https://docs.google.com/spreadsheets/d/' + sheetId + '/gviz/tq?sheet=' + sheetTitle);
 
 function App() {
   const [dataReceived, setDataReceived] = useState({});
   const [state, setState] = useState({});
   const [board, setBoard] = useState([]);
+  const [sheetId, setSheetId] = useState('1BIw0A0lpxnuUmT-uRNFM5bzt4fwTeGbkJEdf7q41L4Q');
+  const [sheetTitle, setSheetTitle] = useState('form1');
+  const [input1, setInput1] = useState('1BIw0A0lpxnuUmT-uRNFM5bzt4fwTeGbkJEdf7q41L4Q');
+  const [input2, setInput2] = useState('form1');
 
   // pull data
   useEffect(() => {
-    const fetchData = async() => {
+    let fulUrl = ('https://docs.google.com/spreadsheets/d/' + sheetId + '/gviz/tq?sheet=' + sheetTitle);
+    const fetchData = () => {
       fetch(fulUrl)
-      .then(res => res.text())
-      .then(rep => {
-        let list = [];
-        let data = JSON.parse(rep.substr(47).slice(0, -2));
-        let list1 = data.table.rows.map((ele) => ele.c);
+        .then(res => res.text())
+        .then(rep => {
+          let list = [];
+          let data = JSON.parse(rep.substr(47).slice(0, -2));
+          let list1 = data.table.rows.map((ele) => ele.c);
 
-        for (let row of list1) {
-          row = row.filter((e) => e !== null && e !== undefined);
-          let temp = row.map((e) => e.v);
-          temp = temp.filter(e => e !== null);
-          list.push(temp);
-        }
-        let newDataReceived = {};
-        for (let x of list){
-          if (newDataReceived[x[1]] === undefined){newDataReceived[x[1]] = {}}
-          newDataReceived[x[1]].name = x[0];
-          if (newDataReceived[x[1]].time === undefined){newDataReceived[x[1]].time = []}
-          newDataReceived[x[1]].time.push(x[2]);
-        }
-        setDataReceived(newDataReceived);
-      })
+          for (let row of list1) {
+            row = row.filter((e) => e !== null && e !== undefined);
+            let temp = row.map((e) => e.v);
+            temp = temp.filter(e => e !== null);
+            list.push(temp);
+          }
+          let newDataReceived = {};
+          for (let x of list) {
+            if (newDataReceived[x[1]] === undefined) { newDataReceived[x[1]] = {} }
+            newDataReceived[x[1]].name = x[0];
+            if (newDataReceived[x[1]].time === undefined) { newDataReceived[x[1]].time = [] }
+            newDataReceived[x[1]].time.push(x[2]);
+          }
+          setDataReceived(newDataReceived);
+        })
+        console.log(1);
     }
     fetchData();
-    setInterval(fetchData, 5000);
-  }, []);
+    let timer = setInterval(fetchData, 5000);
+    return () => clearTimeout(timer);
+  }, [sheetId]);
   useEffect(() => {
-    if (JSON.stringify(state) !== JSON.stringify(dataReceived)){
+    if (JSON.stringify(state) !== JSON.stringify(dataReceived)) {
       setState(dataReceived);
     }
-  }, [dataReceived]);  
+  }, [dataReceived]);
   useEffect(() => {
     const newBoard = [];
-    for (let id in state){
+    for (let id in state) {
       let timeIn = "";
       let timeOut = "";
-      if (state[id].time.length % 2 === 0){
-        timeIn = state[id].time[state[id].time.length-2];
-        timeOut = state[id].time[state[id].time.length-1];
+      if (state[id].time.length % 2 === 0) {
+        timeIn = state[id].time[state[id].time.length - 2];
+        timeOut = state[id].time[state[id].time.length - 1];
       }
-      else{
-        timeIn = state[id].time[state[id].time.length-1];
+      else {
+        timeIn = state[id].time[state[id].time.length - 1];
         timeOut = "---";
       }
       newBoard.push(
@@ -73,15 +76,15 @@ function App() {
 
   const handleClickAll = () => {
     const newBoard = [];
-    for (let id in state){
+    for (let id in state) {
       let timeIn = "";
       let timeOut = "";
-      if (state[id].time.length % 2 === 0){
-        timeIn = state[id].time[state[id].time.length-2];
-        timeOut = state[id].time[state[id].time.length-1];
+      if (state[id].time.length % 2 === 0) {
+        timeIn = state[id].time[state[id].time.length - 2];
+        timeOut = state[id].time[state[id].time.length - 1];
       }
-      else{
-        timeIn = state[id].time[state[id].time.length-1];
+      else {
+        timeIn = state[id].time[state[id].time.length - 1];
         timeOut = "---";
       }
       newBoard.push(
@@ -97,14 +100,14 @@ function App() {
   }
   const handleClickActive = () => {
     const newBoard = [];
-    for (let id in state){
+    for (let id in state) {
       let timeIn = "";
       let timeOut = "";
-      if (state[id].time.length % 2 === 0){
+      if (state[id].time.length % 2 === 0) {
         continue;
       }
-      else{
-        timeIn = state[id].time[state[id].time.length-1];
+      else {
+        timeIn = state[id].time[state[id].time.length - 1];
         timeOut = "---";
       }
       newBoard.push(
@@ -120,14 +123,14 @@ function App() {
   }
   const handleClickLeft = () => {
     const newBoard = [];
-    for (let id in state){
+    for (let id in state) {
       let timeIn = "";
       let timeOut = "";
-      if (state[id].time.length % 2 === 0){
-        timeIn = state[id].time[state[id].time.length-2];
-        timeOut = state[id].time[state[id].time.length-1];
+      if (state[id].time.length % 2 === 0) {
+        timeIn = state[id].time[state[id].time.length - 2];
+        timeOut = state[id].time[state[id].time.length - 1];
       }
-      else{
+      else {
         continue;
       }
       newBoard.push(
@@ -146,16 +149,26 @@ function App() {
   return (
     <div id="App">
       <div id='container'>
-        <div id='header'><h1>BBLab</h1></div>
+        <div id='header'>
+          <div class='input'>
+            <p>Sheet Id</p>
+            <input value={input1} onChange={(e) => setInput1(e.target.value)}></input>
+          </div>
+          <div className='input'>
+            <p>Sheet Title</p>
+            <input value={input2} onChange={(e) => setInput2(e.target.value)}></input>
+          </div>
+          <button onClick={() => {setSheetId(input1); setSheetTitle(input2)}}>Change Sheet</button>
+        </div>
         <div id='buttons'>
-          <button onClick={()=>handleClickAll()}>All</button>
-          <button onClick={()=>handleClickActive()}>Active</button>
-          <button onClick={()=>handleClickLeft()}>Have left</button>
+          <button onClick={() => handleClickAll()}>All</button>
+          <button onClick={() => handleClickActive()}>Active</button>
+          <button onClick={() => handleClickLeft()}>Have left</button>
         </div>
         <div id='board'>
           <table>
             <tbody>
-            <tr>
+              <tr>
                 <td><strong>Name</strong></td>
                 <td><strong>Id</strong></td>
                 <td><strong>Time In</strong></td>
